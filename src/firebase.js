3a5f99665d74324c8de681f833d4cedf77dcc37f
JK-VISWAS <joyviswaskolli@gmail.com>
2026-01-19 14:14:42 +0530

    changed to envorimental rules

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore"; // Use Firestore
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAitf65Rh-3eG-jQ_hjmbElvi7qyH8pFnc", //
-  authDomain: "my-portfolio-1506f.firebaseapp.com", //
-  projectId: "my-portfolio-1506f", //
-  storageBucket: "my-portfolio-1506f.firebasestorage.app", //
-  messagingSenderId: "661902297054", //
-  appId: "1:661902297054:web:61de520753c686613c7f4f" //
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
+  authDomain: "my-portfolio-1506f.firebaseapp.com",
+  projectId: "my-portfolio-1506f",
+  storageBucket: "my-portfolio-1506f.firebasestorage.app",
+  messagingSenderId: "661902297054",
+  appId: "1:661902297054:web:0ab45cd0e7a532363c7f4f",
+  measurementId: "G-BNSXFMDTZ0"
 };
 
 const app = initializeApp(firebaseConfig);
